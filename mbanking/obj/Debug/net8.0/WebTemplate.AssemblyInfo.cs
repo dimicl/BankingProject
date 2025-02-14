74cf4928f4e9bba7387296936a32115958134072
Luka Dimic <lukagaming11@gmail.com>
2025-02-14 16:20:54 +0100

    Izmena

--- mbanking/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
+++ mbanking/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f49e01622602661efabec6068770d60d21bc78d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28641d685a646ddf5ecaca71ff5915a9844cb8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
